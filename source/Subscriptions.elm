ffa6c22a04f5b09333fde5911273ba5a04e2ca53
Chadtech <chadstearns1@gmail.com>
2017-03-06 18:51:26 -0500

    FIRST

---
+++ source/Subscriptions.elm
@@ -0,0 +1,12 @@
+module Subscriptions exposing (subscriptions)
+
+
+import Types exposing (..)
+import Ports exposing (..)
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    fromJS HandlePort
+
+    
